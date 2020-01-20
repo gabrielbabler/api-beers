50b396b0f04d209cddbd6f71c902589730fef2fe
gabrielbabler <gabriel.babler@sensedia.com>
2020-01-20 09:59:37 -0300

    remove unusal import

--- src/main/java/com/craftbeer/api/json/request/BeerRequest.java
+++ src/main/java/com/craftbeer/api/json/request/BeerRequest.java
@@ -1,7 +1,6 @@
 package com.craftbeer.api.json.request;
 
 import com.craftbeer.api.domain.BeerDomain;
-import com.craftbeer.com.json.response.GetBeerResponse;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
